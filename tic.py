d9c3eeb84a114f12ba49186decb4e2734484226f
Uttam <129719033+uttambodara@users.noreply.github.com>
2024-06-22 19:40:38 +0530

    tic.py

--- tic.py
+++ tic.py
@@ -131,3 +131,42 @@ def play_game():
 
 # Driver Code
 print("Winner is: " + str(play_game()))
+
+output:
+
+[[0 0 0]
+ [0 0 0]
+ [0 0 0]]
+Board after 1 move
+[[0 0 0]
+ [0 0 0]
+ [1 0 0]]
+Board after 2 move
+[[0 0 0]
+ [0 2 0]
+ [1 0 0]]
+Board after 3 move
+[[0 1 0]
+ [0 2 0]
+ [1 0 0]]
+Board after 4 move
+[[0 1 0]
+ [2 2 0]
+ [1 0 0]]
+Board after 5 move
+[[1 1 0]
+ [2 2 0]
+ [1 0 0]]
+Board after 6 move
+[[1 1 0]
+ [2 2 0]
+ [1 2 0]]
+Board after 7 move
+[[1 1 0]
+ [2 2 0]
+ [1 2 1]]
+Board after 8 move
+[[1 1 0]
+ [2 2 2]
+ [1 2 1]]
+Winner is: 2
